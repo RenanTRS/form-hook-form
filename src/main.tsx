4a42430c9428390d896bf94a69c18113206962c3
Renan Souza <renantrsouza@gmail.com>
2022-03-08 12:04:37 -0400

    Fixed theme

--- src/main.tsx
+++ src/main.tsx
@@ -1,16 +1,16 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import {ThemeProvider} from 'styled-components'
+import theme from './style/theme/themeForm'
 import { Reset } from './style/Reset'
 import { GlobalStyle } from './style/GlobalStyle'
-import {ThemeProvider} from 'styled-components'
-import theme from './style/theme'
 import App from './App'
 
 ReactDOM.render(
   <React.StrictMode>
-    <Reset />
-    <GlobalStyle />
     <ThemeProvider theme={theme}>
+      <Reset />
+      <GlobalStyle />
       <App />
     </ThemeProvider>
   </React.StrictMode>,
