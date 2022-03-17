fd9fec67f9840b9d62981eeb3f45c452ef933dfa
Renan Souza <renantrsouza@gmail.com>
2022-03-17 15:21:40 -0400

    Add functions for dinamic select

--- src/components/Form/index.tsx
+++ src/components/Form/index.tsx
@@ -6,23 +6,16 @@ import {Container} from './style'
 import { Field } from './Field'
 import { useMask } from '../../hooks/useMask'
 import { useFetch } from '../../hooks/useFetch'
-import { FormEvent, useCallback } from 'react'
+import { FormEvent, useCallback, useEffect, useState } from 'react'
 import { SelectBlock } from './Field/style';
+import { useUf } from '../../hooks/useUf';
+import { useCity } from '../../hooks/useCity';
+import { FormInputType } from '../../types/Form';
+import { UfType } from '../../types/Form';
+import { CitiesType } from '../../types/Form';
 
 export const Form = () => {
-
-    type FormInputType = {
-        name?: string;
-        email?: string;
-        cpf?: string;
-        cep?: string;
-        uf?: string;
-        city?: string;
-        district?: string;
-        street?: string;
-        number?: string;
-    }
-    const {register, handleSubmit, watch, formState: {errors}} = useForm<FormInputType>({resolver: yupResolver(schema)});
+    const {register, handleSubmit, formState: {errors}} = useForm<FormInputType>({resolver: yupResolver(schema)});
     
     const newUser = (data: FormInputType) => {
         console.log(data)
@@ -31,14 +24,37 @@ export const Form = () => {
         useMask(event)
     },[])
 
+    const populateUf = async () => {
+        const response = await useUf('https://servicodados.ibge.gov.br/api/v1/localidades/estados')
+        setUfs(response)
+    }
+    const populateCity = async (uf: string) => {
+        const response = await useCity(`https://servicodados.ibge.gov.br/api/v1/localidades/estados/${uf}/distritos`)
+        setCities(response)
+    }
+    useEffect(()=>{
+        populateUf()
+    },[])
+    const [ufs, setUfs] = useState<UfType[]>([])
+    const [city, setCity] = useState<boolean>(false)
+    const [cities, setCities] = useState<CitiesType[]>([])
+    
+    const populate = (event: FormEvent<HTMLSelectElement>) => {
+        const {value} = event.currentTarget
+        populateCity(value)
+        setCity(true)
+    }
+    
     const getData = async (event: FormEvent<HTMLInputElement>) => {
         let {value} = event.currentTarget
         value = value.replace(/\D/g, '')
         if(value.length === 8){
             const data = await useFetch(`https://viacep.com.br/ws/${value}/json/`)
-            //console.log(data)
+            console.log(data)
         }
+        //Pendente ....
     }
+    
 
     return (
         <Container onSubmit={handleSubmit(newUser)}>
@@ -48,12 +64,29 @@ export const Form = () => {
 
             <Field.Text label={'cpf'} labelName={'CPF'} register={register('cpf')} error={errors.cpf?.message} onchange={handleMask} maxlength={14} />
 
-            <Field.Text label={'cep'} labelName={'CEP'} register={register('cep',{onBlur:getData})} error={errors.cep?.message} onchange={handleMask} maxlength={9} />
+            <Field.Text label={'cep'} labelName={'CEP'} register={register('cep', {onBlur:getData})} error={errors.cep?.message} onchange={handleMask} maxlength={9} />
 
             <SelectBlock>
-                <Field.Select label={'uf'} labelName={'Estado'} register={register('uf')} />
-
-                <Field.Select label={'city'} labelName={'Cidade'} register={register('city')} />
+                <Field.Select label={'uf'} labelName={'Estado'} register={register('uf')} populate={(event)=>populate(event)}>
+                    <option value="">Selecione um estado</option>
+                    {ufs &&
+                        ufs.map((uf)=>{
+                            return(
+                                <option key={uf.id} value={uf.sigla}>{uf.nome}</option>
+                            )
+                        })
+                    }
+                </Field.Select>
+                <Field.Select label={'city'} labelName={'Cidade'} register={register('city')}>
+                    <option value="">Selecione uma cidade</option>
+                    {city &&
+                        cities.map((city)=>{
+                            return(
+                                <option key={city.id} value={city.nome}>{city.nome}</option>
+                            )
+                        })
+                    }
+                </Field.Select>
             </SelectBlock>
 
             <Field.Text label={'district'} labelName={'Bairro'} register={register('district')} />
