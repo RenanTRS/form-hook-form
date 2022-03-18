cf9a8ac381f78d843655e675d5dc94d805f3b97f
Renan Souza <renantrsouza@gmail.com>
2022-03-18 17:02:50 -0400

    Use context

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
 import { Main } from "./components/Main"
 import { Form } from "./components/Form"
+import { SelectContextProvider } from "./context/SelectContext"
 
 function App() {
   return (
     <Main>
-      <h2>Form Hook Form</h2>
-      <Form />
+      <SelectContextProvider>
+        <h2>Form Hook Form</h2>
+        <Form />
+      </SelectContextProvider>
     </Main>
   )
 }
