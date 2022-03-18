99577d9d4a0917bb1c8aef45f0408f7b6031a6d1
Renan Souza <renantrsouza@gmail.com>
2022-03-18 09:57:26 -0400

    Add style for button

--- src/components/Form/style.ts
+++ src/components/Form/style.ts
@@ -10,4 +10,26 @@ export const Container = styled.form`
     border-radius: 1rem;
 
     width: 50%;
+
+    button {
+        background: ${props => props.theme.colors.submit};
+        
+        font-weight: bold;
+        font-size: 2rem;
+        color: ${props => props.theme.colors.submitText};
+
+        border: none;
+        border-radius: 1.5rem;
+
+        height: 4rem;
+        width: 50%;
+
+        margin: 1rem auto;
+
+        cursor: pointer;
+
+        &:active {
+            background: ${props => props.theme.colors.submitHover};
+        }
+    }
 `
