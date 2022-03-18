4eaa317a8cba7382c0047e65f713d862f6a34546
Renan Souza <renantrsouza@gmail.com>
2022-03-18 17:08:40 -0400

    Refactor and remove functions

--- src/components/Form/Field/Selects/index.tsx
+++ src/components/Form/Field/Selects/index.tsx
@@ -3,28 +3,23 @@ import { FormEvent, useEffect, useContext } from "react"
 import { SelectContext } from "../../../../context/SelectContext"
 import { useUf } from "../../../../hooks/useUf"
 import { Field } from ".."
-import { useCity } from "../../../../hooks/useCity"
-
 
 export const SelectState = ({label, labelName, register}: InputProps) => {    
-    const {setCity, ufs, setUfs, setCities} = useContext(SelectContext)
+    const {ufs, setUfs, populateCity} = useContext(SelectContext) //Context
     
-    const populateUf = async () => {
+    const populateUf = async () => { //Take states
         const response = await useUf('https://servicodados.ibge.gov.br/api/v1/localidades/estados')
         setUfs(response)
     }
-    const populateCity = async (uf: string) => {
-        const response = await useCity(`https://servicodados.ibge.gov.br/api/v1/localidades/estados/${uf}/distritos`)
-        setCities(response)
-    }
+
     const populate = (event: FormEvent<HTMLSelectElement>) => {
         const {value} = event.currentTarget
-        populateCity(value)
-        setCity(true)
+        populateCity(value) //Take cities
     }
+
     useEffect(()=>{
         populateUf()
-    },[])
+    }, [])
 
     return (
         <Field.Select label={label} labelName={labelName} register={register} populate={(event) => populate(event)}>
@@ -41,12 +36,13 @@ export const SelectState = ({label, labelName, register}: InputProps) => {
 }
 
 export const SelectCity = ({label, labelName, register}: InputProps) => {
-    const {city, cities} = useContext(SelectContext)
+    const {cities} = useContext(SelectContext)
+
     return (
         <Field.Select label={label} labelName={labelName} register={register}>
             <option value="">Selecione uma Cidade</option>
-            {city &&
-                cities?.map(city => {
+            {cities &&
+                cities.map(city => {
                     return (
                         <option key={city.id} value={city.nome}>{city.nome}</option>
                     )
