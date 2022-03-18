c1da0851ec8de8acf5f0785ac55d97f79a038468
Renan Souza <renantrsouza@gmail.com>
2022-03-18 17:02:18 -0400

    Add function

--- src/context/SelectContext.tsx
+++ src/context/SelectContext.tsx
@@ -1,4 +1,5 @@
 import { createContext, useState } from "react"
+import { useCity } from "../hooks/useCity";
 import { IContextProps, ISelectContext } from "../interfaces/Form";
 import { CitiesType, UfType } from "../types/Form"
 
@@ -6,10 +7,14 @@ export const SelectContext = createContext({} as ISelectContext)
 
 export const SelectContextProvider = ({children}: IContextProps) => {
     const [ufs, setUfs] = useState<UfType[]>([])
-    const [city, setCity] = useState<boolean>(false)
     const [cities, setCities] = useState<CitiesType[]>([])
     
-    const value = {ufs, setUfs, city, setCity, cities, setCities}
+    const populateCity = async (uf:string) => {
+        const response = await useCity(`https://servicodados.ibge.gov.br/api/v1/localidades/estados/${uf}/distritos`)
+        setCities(response)
+    }
+
+    const value = {ufs, setUfs, cities, setCities, populateCity}
     
     return (
         <SelectContext.Provider value={value}>
